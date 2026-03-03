b5b09b71f4f92c34018f2a0af394c6642b9edf49
qawsedrftgyhujikolpmnbvcxz <gudurua5933@my.sduhsd.net>
2026-03-03 21:34:37 +0000

    saving changes

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// add javascript here
+document.getElementById("ansbtn")?.addEventListener("click", () => {
+    let answer = document.getElementById("answer")
+    let list = document.getElementById("submittedlist")
+    if(answer.value.trim()!=""){
+        const newAnswer = document.createElement("li");
+        newAnswer.textContent = answer.value
+        list.appendChild(newAnswer);
+        answer.value = ""
+    }
+})
+
+document.getElementById("coolbtn")?.addEventListener("click", () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+})
